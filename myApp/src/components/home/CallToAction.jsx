3e05ea1b8c6b9388a710a1591f161bc9bb9f3b29
saivivek122 <119473212+saivivek122@users.noreply.github.com>
2026-05-07 11:57:55 +0530

    Remove unnecessary closing anchor tag in CallToAction
    Removed an unnecessary closing anchor tag.

--- myApp/src/components/home/CallToAction.jsx
+++ myApp/src/components/home/CallToAction.jsx
@@ -12,7 +12,7 @@ const CallToAction = () => {
   <span>Get Started</span>
 </Link>
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="size-4.5"><path d="M5 12h14"/><path d="m12 5 7 7-7 7"/></svg>
-                </a>
+               
             </div>
         </div>
   )
